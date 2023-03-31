714de6b3a4ae111018c7c103f1f4b1cc1ed23eb4
tekater <120014627+tekater@users.noreply.github.com>
2023-03-31 20:33:49 +0900

    Update SortArray.cpp

--- SortArray.cpp
+++ SortArray.cpp
@@ -5,10 +5,10 @@
 #include <time.h>
 using namespace std;
 
-const int length = 100;
+const int length = 25;
 
 template <class T>
-void _selectsort(T arr[], int length) {
+void SelSort(T arr[], int length) {
 	int i, j, k; // i - номер текущего шага
 	T x;
 	for (i = 0; i < length; i++)
@@ -32,7 +32,7 @@ void _selectsort(T arr[], int length) {
 }
 
 template <class T>
-void _selectsort2(T arr[], int length) {
+void SelSort2(T arr[], int length) {
 	int min = 0;
 	int temp = 0;
 
@@ -67,19 +67,40 @@ void BubbleSort(T arr[], int length) {
 	}
 }
 
+template <class T>
+void InsertionSort(T arr[], int length) {
+	int temp = 0;	  // Сохранение перемещаемого значения.
+	int i, j;		  // Для циклов.
+	for (i = 1; i < length; i++)
+	{
+		temp = arr[i];// Запоминаем элемент и начнем перемещение элемента, слева от него.
+					  // до тех пор, пока запоминаемый элемент не окажется меньше, чем переменная.
+		for (j = i - 1; j >= 0 && arr[j] > temp;j--) {
+			arr[j + 1] = arr[j];
+			arr[j] = temp; // Ставим элемент на его новое место.
+		}
+	}
+}
+
 int main()
 {
 	setlocale(0, "");
 	srand(time(NULL));
-	system("color B5");
+	//system("color B5");
 
 	int arr[length] = { 1,0,0,0,1 };
 	for (int i = 0; i < length; i++) {
 		arr[i] = 1 + rand() % 100;
 	}
-	//_selectsort(arr,length);
-	//_selectsort2(arr,length);
-	BubbleSort(arr, length);
+	cout << "Изначальный Массив: ";
+	for (int i = 0; i < length; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl << "\nСортировка: ";
+	//SelSort(arr,length);
+	//SelSort2(arr,length);
+	//BubbleSort(arr, length);
+	InsertionSort(arr, length);
 
 	for (int i = 0; i < length; i++) {
 		cout << arr[i] << " "; Sleep(100);// красивое
